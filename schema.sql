6fc256cab721d7e8347dc50599c3d6be2166b7fc
John yeager <johnyeager@Johns-MacBook-Pro.local>
2018-05-08 20:50:21 -0400

    fixed sql

--- schema.sql
+++ schema.sql
@@ -7,5 +7,6 @@ id INTEGER(11) AUTO_INCREMENT,
 product_name VARCHAR(50) NOT NULL,
 department_name VARCHAR(50) NOT NULL,
 price NUMERIC(4, 2) NOT NULL,
-stock_quantity NUMERIC(11) NOT NULL
+stock_quantity NUMERIC(11) NOT NULL,
+PRIMARY KEY(id)
 );
